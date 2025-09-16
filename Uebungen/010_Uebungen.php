c26273cfe972532acc497aaf2d09c9851da7a224
drayrida <sandraschuster8699@gmail.com>
2025-09-16 15:32:11 +0200

    CSS_Newstruktur

---
+++ Uebungen/010_Uebungen.php
@@ -0,0 +1,14 @@
+<?php
+/*
+* Eigenen Namen ausgeben
+*
+* Erstelle je eine Variable für Vornamen,
+* Nachnamen und Wohnort und gib dann folgendes aus:
+* X Y wohnt in Z.
+*/
+
+$vorname = 'Sandra';
+$nachname = 'Schuster';
+$wohnort = 'Germany';
+
+print"My name is $vorname and my lastname is $nachname and i live in $wohnort.";
