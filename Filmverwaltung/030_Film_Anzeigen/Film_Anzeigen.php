de8fd7296fba934521dd832fb39b94138cdb91eb
drayrida <sandraschuster8699@gmail.com>
2025-09-22 08:53:49 +0200

    new

--- Filmverwaltung/030_Film_Anzeigen/Film_Anzeigen.php
+++ Filmverwaltung/030_Film_Anzeigen/Film_Anzeigen.php
@@ -1,1 +1,24 @@
 <?php
+$options = [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+
+];
+
+var_dump($_GET);
+echo $_GET['id']
+
+?>
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <meta charset="UTF-8">
+    <title>Film anzeigen</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<h1>Film anzeigen</h1>
+
+</body>
+</html>
+
